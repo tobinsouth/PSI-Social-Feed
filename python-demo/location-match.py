889a2242176f946199d30c05e0a3afae293111be
Tobin South <tobin.south@gmail.com>
2022-09-29 20:14:22 -0400

    Fixing boilerplate files

--- python-demo/location-match.py
+++ python-demo/location-match.py
@@ -1,3 +1,3 @@
 """
-This file demostraigthss how to load in a simple set of GPS sequences, geohash them, and private match them between two parties without any unencrypted sharing or trusted third party needed.
+This file demonstrates how to load in a simple set of GPS sequences, geohash them, and private match them between two parties without any unencrypted sharing or trusted third party needed.
 """
