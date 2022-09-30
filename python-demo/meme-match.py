6e191c87c44fa81b1108079a67869e6349375004
Tobin South <tobin.south@gmail.com>
2022-09-29 20:13:00 -0400

    Adding python boilerplates

---
+++ python-demo/meme-match.py
@@ -0,0 +1,3 @@
+"""
+This file demostraigthss how to load in a set of images, perceputally hash them, and private match them between two parties without any unencrypted sharing or trusted third party needed.
+"""
